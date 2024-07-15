bfa10e1023f770eff4ef63e6017d5d51aeb13721
omkar4289 <100528610+omkar4289@users.noreply.github.com>
2024-07-15 16:55:31 +0530

    Update Date2.java

--- src/Date2.java
+++ src/Date2.java
@@ -2,7 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+// comment
 public class Date2 {
 
 	public static void main(String[] args) throws InterruptedException {
